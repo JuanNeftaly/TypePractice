0663c8d6a8c28358babecad501a46eacdf8fd752
JuanNeftaly <00182222@uca.edu.sv>
2025-10-09 11:54:49 -0600

    Add: Adding dummyData

---
+++ src/assets/data/todos.ts
@@ -0,0 +1,13 @@
+export const dummyData = [
+  {
+    id: 1,
+    title: "Linganguli guli wacha",
+    completed: false,
+  },
+  { id: 2, title: "Terminar practica de react typescript", completed: false },
+  {
+    id: 3,
+    title: "Sacar la basura",
+    completed: false,
+  },
+];
